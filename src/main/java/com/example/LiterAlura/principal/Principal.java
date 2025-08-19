0bb137e0c7fb03718cbac9f101a90b1f05b65116
Rodrigo Everton <rodrigoeverton1500@gmail.com>
2025-08-19 10:07:20 -0300

    api service

---
+++ src/main/java/com/example/LiterAlura/principal/Principal.java
@@ -0,0 +1,34 @@
+package com.example.LiterAlura.principal;
+
+import com.example.LiterAlura.service.ConsumoAPI;
+import com.example.LiterAlura.service.ConverteDados;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoAPI consumo = new ConsumoAPI();
+    private ConverteDados conversor = new ConverteDados();
+    private final String ENDERECO = "https://gutendex.com/";
+
+    public void exibeMenu() {
+        var opcao = -1;
+        while(opcao != 0) {
+            var menu = """
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
+                    4 - Buscar série por título
+                    5 - Buscar séries por ator
+                    6 - Top 5 Séries
+                    7 - Buscar séries por categoria
+                    8 - Filtrar séries
+                    9 - Buscar episódios por trecho
+                    10 - Top 5 episódios por série
+                    11 - Buscar episódios a partir de uma data 
+                                    
+                    0 - Sair                                 
+                    """;
+        }
+    }
+}
